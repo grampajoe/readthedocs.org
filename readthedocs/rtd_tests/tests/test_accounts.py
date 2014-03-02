4513a68260cdf8a8c2ab48a2a54f0daa48996061
Joe Friedl <joe@joefriedl.net>
2014-03-02 16:43:23 -0500

    Add some basic tests for the current account system

---
+++ readthedocs/rtd_tests/tests/test_accounts.py
@@ -0,0 +1,81 @@
+from django.core import mail
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
+from registration.models import RegistrationProfile
+
+
+class AccountRegistrationTests(TestCase):
+
+    def setUp(self):
+        self.username = 'testuser'
+        self.email = 'test@test.com'
+        self.password = 'test_password'
+
+    def register(self):
+        """POSTs to the registration URL."""
+        return self.client.post('/accounts/register/', {
+            'username': self.username,
+            'email': self.email,
+            'password1': self.password,
+            'password2': self.password,
+        })
+
+    def test_register_account(self):
+        """
+        Test that users can use the login page to register.
+        """
+        response = self.register()
+
+        user = User.objects.get(username=self.username)
+
+        self.assertEqual(user.username, self.username)
+        self.assertEqual(user.email, self.email)
+        self.assertFalse(user.is_active)
+
+    def test_sends_activation_email(self):
+        """
+        Test that users get activation emails.
+        """
+        self.register()
+
+        self.assertEqual(len(mail.outbox), 1)
+
+        message = mail.outbox[0]
+        self.assertIn(self.email, message.to)
+        self.assertIn('activate', message.subject.lower())
+
+
+class AccountMigrationTests(TestCase):
+
+    def setUp(self):
+        self.username = 'testuser'
+        self.email = 'test@test.com'
+        self.password = 'test_password'
+
+    def test_old_activation_links_still_work(self):
+        """
+        Test that django-registration activation links can still be used.
+
+        This test can be safely removed once the migration to django-allauth
+        is complete and no outstanding activation links exist. See
+        settings.ACCOUNT_ACTIVATION_DAYS to find out when that will be.
+        """
+        user = RegistrationProfile.objects.create_inactive_user(
+            username=self.username,
+            email=self.email,
+            password=self.password,
+            site=Site.objects.get_current(),
+        )
+        registration = RegistrationProfile.objects.get(user=user)
+
+        self.assertFalse(user.is_active)
+
+        self.client.get(reverse(
+            'registration_activate',
+            kwargs={'activation_key': registration.activation_key},
+        ))
+
+        self.assertTrue(User.objects.get(id=user.id).is_active)
